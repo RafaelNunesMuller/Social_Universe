8c5052a662ff9e0839bbbf477bfbc87566784df4
RafaelNunesMuller <rafaelmuller007@icloud.com>
2025-05-08 15:17:06 -0300

    .

--- SocialUniverse/Assets/Player/PlayerMove.cs
+++ SocialUniverse/Assets/Player/PlayerMove.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine.EventSystems;
 
 public class PlayerMove : MonoBehaviour
 {
     public float Speed;
     public float Gravity;
+<<<<<<< Updated upstream
+=======
+    public float JumpForce = 5f; // Adicionamos a força do pulo
+    public bool canRotate = true;
+    public bool canJump = true; // Adicionamos esta variável para controlar o pulo
+    private Animator anim;
+    private Vector3 MoveDirection;
+>>>>>>> Stashed changes
 
     private Vector3 _moveDirection;
     private CharacterController _controller;
@@ -45,6 +54,7 @@ public class PlayerMove : MonoBehaviour
 
             if (Input.GetKey(KeyCode.W))
             {
+<<<<<<< Updated upstream
                 if (cinemachineCamera != null)
                 {
                     // Obtém a rotação Y da câmera Cinemachine
@@ -62,9 +72,35 @@ public class PlayerMove : MonoBehaviour
                     Debug.LogWarning("Cinemachine Camera não está atribuída, usando direção para frente local.");
                     _moveDirection = transform.forward * Speed;
                 }
+=======
+                float cameraYaw = cinemachineCamera.transform.eulerAngles.y;
+                Quaternion targetRotation = Quaternion.Euler(0f, cameraYaw, 0f);
+                transform.rotation = targetRotation;
+                _moveDirection.x = transform.forward.x * Speed; // Move na direção horizontal
+                _moveDirection.z = transform.forward.z * Speed; // Move na direção vertical (no plano horizontal)
+                anim.SetInteger("transition", 1);
             }
+            else if (cinemachineCamera == null)
+            {
+                Debug.LogWarning("Cinemachine Camera não está atribuída, usando direção para frente local.");
+                _moveDirection.x = transform.forward.x * Speed;
+                _moveDirection.z = transform.forward.z * Speed;
+>>>>>>> Stashed changes
+            }
+            
         }
 
+<<<<<<< Updated upstream
+=======
+        // Lógica de pulo (só pode pular se estiver no chão)
+        if (_controller.isGrounded && canJump && Input.GetButtonDown("Jump"))
+        {
+            _moveDirection.y = JumpForce;
+            anim.SetInteger("transition", 2);
+        }
+
+        // Aplica a gravidade
+>>>>>>> Stashed changes
         _moveDirection.y -= Gravity * Time.deltaTime;
         _controller.Move(_moveDirection * Time.deltaTime);
     }
